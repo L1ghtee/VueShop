a4b5212b725700d0da42f8a5a4ea52b5a22dac23
L1ghtee <nikorak20@gmail.com>
2025-01-07 13:18:17 +0200

    create store (video 10:23)

---
+++ src/views/Catalog.vue
@@ -0,0 +1,21 @@
+<template>
+ <h1> Catalog</h1>
+</template>
+
+<script>
+import { defineComponent } from "vue";
+export default defineComponent({
+  name: "CatalogView",
+});
+</script>
+
+<script setup>
+import {onMounted} from 'vue';
+import{productsStore} from "@/stores/products";
+const productStore = productsStore();
+
+onMounted(() => {
+  console.log('CatalogView mounted')
+  productStore.fetchProductsFromDB()
+})
+</script>
